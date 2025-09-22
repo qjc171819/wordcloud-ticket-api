de3130fa59df7ebfc9495ea331e3fe17a2128daa
qjc171819 <46129422+qjc171819@users.noreply.github.com>
2025-09-22 10:19:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,17 @@ logger = logging.getLogger(__name__)
 
 # base64图片转url
 def generate_url(base64_image):
-    postUrl = r'https://api.imgbb.com/1/upload'
-    api_key = 'cd252b3a315af679db9b6f10dbe1eff9'
+    # postUrl = r'https://api.imgbb.com/1/upload'
+    # api_key = 'cd252b3a315af679db9b6f10dbe1eff9'
+    postUrl = r'https://freeimage.host/api/1/upload'
+    api_key = '6d207e02198a847aa98d0a2a901485a5'
     # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0'
     req = requests.post(f'{postUrl}?key={api_key}', data = {'image': base64_image}
                         #,headers = {'user-agent': user_agent}
                         )
     js = req.json()
-    image_url = js['data']['image']['url']
+    # image_url = js['data']['image']['url']
+    image_url = js['image']['url']
     return image_url
 
 
@@ -316,6 +319,7 @@ def generate_wordcloud():
 if __name__ == '__main__':
     # 生产环境应设置debug=False
     app.run(host='0.0.0.0', port=5080, debug=False)
+
 
 
 
